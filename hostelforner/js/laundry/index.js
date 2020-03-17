af6d88e4e804cd82c0d89280d37bb86fcc713ddc
Ismael Ribeiro Moura <corage_thedogcowardle@hotmail.com>
2020-03-16 23:57:07 -0300

    Alteração de nomes

--- hostelforner/js/laundry/index.js
+++ hostelforner/js/laundry/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-    var dados = [{diaSemana:"Segunda", descricao:"Rubens Neto Rafael"}, 
-                {diaSemana:"Terça", descricao:"Henrique Cris Márcia"},
-                {diaSemana:"Quarta", descricao:"Larissa Guilherme Alessandra"},
-                {diaSemana:"Quinta", descricao:"Hadryel Filipe Sabrina"},
-                {diaSemana:"Sexta", descricao:"Edgar Breno Mariana"},
-                {diaSemana:"Sábado", descricao:"Ismael Wallison Johnny"},
+    var dados = [{diaSemana:"Segunda", descricao:"Rafael Neto Rubens Hadryel"}, 
+                {diaSemana:"Terça", descricao:"Henrique Cris Márcia Fábio"},
+                {diaSemana:"Quarta", descricao:"Larissa Guilherme Alessandra Cloves Cezar"},
+                {diaSemana:"Quinta", descricao:"Filipe Sabrina Bruno Larissa Cavalcante"},
+                {diaSemana:"Sexta", descricao:"Juliana Breno Mariana Isabella Jéssica Dulce"},
+                {diaSemana:"Sábado", descricao:"Edgar Ismael Wallison Johnny"},
                 {diaSemana:"Domingo", descricao:"NÃO USAR"}]
 
     $(dados).each(function (i) {
