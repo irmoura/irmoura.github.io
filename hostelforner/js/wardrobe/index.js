4a0d29481ec703682ef4efe309558479ba5ae61e
Ismael Ribeiro Moura <corage_thedogcowardle@hotmail.com>
2020-03-17 16:27:31 -0300

    Alterações simples

--- hostelforner/js/wardrobe/index.js
+++ hostelforner/js/wardrobe/index.js
@@ -1,22 +1,43 @@
 $(document).ready(function () {
 
     var wardrobe1 = [
-        ['', 'Guilherme', '', 'Rubens'],
-        ['Alessandra', 'Sabrina', 'Ismael', 'Breno'],
+        ['', 'Guilherme', 'Johnny', 'Rubão'],
+        ['Larissa C.', 'Juliana', 'Ismael R.', 'Breno'],
         ['Bruno', 'Márcia', 'Neto', 'Criss'],
-        ['Henrique', 'Larissa', 'Mariana', 'Juliana'],
-        ['Filipe', '', 'Fábio', 'Hadryel']
+        ['Rick', 'Larissa', 'Mariana', 'Juliana Motty'],
+        ['Filipe', 'Cesar', 'Fábio', 'Hadryel']
     ];
 
-    var html_ = ``;
+    var html_1 = ``;
 
     for(var i = 0; i < 5; i++){
-        html_ += `<tr>`;
+        html_1 += `<tr>`;
         for(var j = 0; j < 4; j++){
-            html_ += `<td>${wardrobe1[i][j]}</td>`;
+            html_1 += `<td>${wardrobe1[i][j]}</td>`;
         };
-        html_ += `</tr>`;
+        html_1 += `</tr>`;
     };
 
-    $('#wardrobeTable').append(html_);
+    $('#wardrobe1').append(html_1);
+
+    //
+
+    var wardrobe2 = [
+        ['Edvaldo', 'Clovis'],
+        ['Lígia', 'Sabrina'],
+        ['Jéssica', 'Isabella'],
+        ['', '']
+    ];
+
+    var html_2 = ``;
+
+    for(var i = 0; i < 4; i++){
+        html_2 += `<tr>`;
+        for(var j = 0; j < 2; j++){
+            html_2 += `<td>${wardrobe2[i][j]}</td>`;
+        };
+        html_2 += `</tr>`;
+    };
+
+    $('#wardrobe2').append(html_2);
 });
