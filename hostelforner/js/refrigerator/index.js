38552640577a95b87e1385d5d3c611262f8f1c6c
Ismael Ribeiro Moura <corage_thedogcowardle@hotmail.com>
2020-03-19 16:45:21 -0300

    Uso de API na parte Refrigerator

--- hostelforner/js/refrigerator/index.js
+++ hostelforner/js/refrigerator/index.js
@@ -1,63 +1,88 @@
+var refrigerator1 = [];
+var refrigerator2 = [];
+var refrigerator3 = [];
+var html = ``;
+var count = 1;
+
 $(document).ready(function () {
-
-    var html1 = ``;
-    var html2 = ``;
-    var html3 = ``;
-
-    var refrigerator1 = [
-        ['Johnny', 'Cris'],
-        ['Henrique', 'Márcia'],
-        ['Larissa', 'Mariana']
-    ];
-
-    var refrigerator2 = [
-        ['Guilherme', 'Cesar'],
-        ['Rubens', 'Neto'],
-        ['Hadryel', 'Filipe'],
-        ['Edgar', 'Bruno']
-    ];
-
-    var refrigerator3 = [
-        ['Juliana Abreu', 'Edvaldo'],
-        ['Fábio', 'Ismael'],
-        ['Juliana Dantas', 'Sabrina'],
-        ['Clovis', 'Larissa Cavalcanti'],
-        ['Isabella', 'Jéssica Durce']
-    ];
-
-    for(var i = 0; i < 3; i++){
-        html1 += `<tr>`;
-        for(var j = 0; j < 2; j++){
-            html1 += `<td>${refrigerator1[i][j]}</td>`;
+    var jqxhr = $.get( "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/sisa-hwgco/service/api/incoming_webhook/webhook0", function(data) {
+        $(data).each(function (i) {
+            var haveRefrigerator = data[i].HospedeTemGeladeira;
+            if(haveRefrigerator){
+                if(data[i].HospedeGeladeira < 7){
+                    var oItem1 = {
+                        Nome: data[i].HospedeNome,
+                        Lugar: `${data[i].HospedeGeladeira}`
+                    }
+                    refrigerator1.push(oItem1)
+                }
+                if(data[i].HospedeGeladeira < 15){
+                    var oItem2 = {
+                        Nome: data[i].HospedeNome,
+                        Lugar: `${data[i].HospedeGeladeira}`
+                    }
+                    refrigerator2.push(oItem2)
+                }else{
+                    var oItem3 = {
+                        Nome: data[i].HospedeNome,
+                        Lugar: `${data[i].HospedeGeladeira}`
+                    }
+                    refrigerator3.push(oItem3)
+                }
+            }
+        });
+    }).done(function() {
+        //
+        for(var i = 0; i < 3; i++){
+            html += `<tr>`;
+            for(var j = 0; j < 2; j++){
+                var index = refrigerator1.findIndex(a => a.Lugar == `${count}`);
+                if(index === -1){
+                    html += `<td></td>`;
+                }else{
+                    var nome = refrigerator1[index].Nome
+                    html += `<td>${nome}</td>`;
+                }
+                count++;
+            };
+            html += `</tr>`;
         };
-        html1 += `</tr>`;
-    };
-
-    $('#refrigerator1').append(html1);
-
-    //
-
-    for(var i = 0; i < 4; i++){
-        html2 += `<tr>`;
-        for(var j = 0; j < 2; j++){
-            html2 += `<td>${refrigerator2[i][j]}</td>`;
+        $('#refrigerator1').append(html);
+        //
+        html = ``;
+        //
+        for(var i = 0; i < 4; i++){
+            html += `<tr>`;
+            for(var j = 0; j < 2; j++){
+                var index = refrigerator2.findIndex(a => a.Lugar == `${count}`);
+                if(index === -1){
+                    html += `<td></td>`;
+                }else{
+                    var nome = refrigerator2[index].Nome
+                    html += `<td>${nome}</td>`;
+                }
+                count++;
+            };
+            html += `</tr>`;
         };
-        html2 += `</tr>`;
-    };
-
-    $('#refrigerator2').append(html2);
-
-    //
-
-    for(var i = 0; i < 5; i++){
-        html3 += `<tr>`;
-        for(var j = 0; j < 2; j++){
-            html3 += `<td>${refrigerator3[i][j]}</td>`;
+        $('#refrigerator2').append(html);
+        //
+        html = ``;
+        //
+        for(var i = 0; i < 5; i++){
+            html += `<tr>`;
+            for(var j = 0; j < 2; j++){
+                var index = refrigerator3.findIndex(a => a.Lugar == `${count}`);
+                if(index === -1){
+                    html += `<td></td>`;
+                }else{
+                    var nome = refrigerator3[index].Nome
+                    html += `<td>${nome}</td>`;
+                }
+                count++;
+            };
+            html += `</tr>`;
         };
-        html3 += `</tr>`;
-    };
-
-    $('#refrigerator3').append(html3);
-
-    //
+        $('#refrigerator3').append(html);
+    });
 });
